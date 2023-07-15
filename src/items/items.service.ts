d4f45d3fc914e7217ff45e06b9505dc8a52d14d1
cuter97 <aguscastets@gmail.com>
2023-07-15 00:08:30 -0300

    add createItem and entity

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,11 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { CreateItemInput } from './dto/create-item.input';
 import { UpdateItemInput } from './dto/update-item.input';
+import { Item } from './entities/item.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ItemsService {
-  create(createItemInput: CreateItemInput) {
-    return 'This action adds a new item';
+
+  constructor(
+    @InjectRepository(Item)
+    private readonly itemsRepository: Repository<Item>
+  ) {}
+
+  async create(createItemInput: CreateItemInput): Promise<Item> {
+    const newItem = this.itemsRepository.create(createItemInput);
+    return await this.itemsRepository.save(newItem);
   }
 
   findAll() {
