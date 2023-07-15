5a71405e45c97e8643991cff46ecc41761568518
cuter97 <aguscastets@gmail.com>
2023-07-15 00:09:57 -0300

    space

--- src/items/items.resolver.ts
+++ src/items/items.resolver.ts
@@ -6,30 +6,32 @@ import { UpdateItemInput } from './dto/update-item.input';
 
 @Resolver(() => Item)
 export class ItemsResolver {
-  constructor(private readonly itemsService: ItemsService) {}
+    constructor(private readonly itemsService: ItemsService) { }
 
-  @Mutation(() => Item)
-  createItem(@Args('createItemInput') createItemInput: CreateItemInput) {
-    return this.itemsService.create(createItemInput);
-  }
+    @Mutation(() => Item)
+    async createItem(
+        @Args('createItemInput') createItemInput: CreateItemInput
+    ): Promise<Item> {
+        return this.itemsService.create(createItemInput);
+    }
 
-  @Query(() => [Item], { name: 'items' })
-  findAll() {
-    return this.itemsService.findAll();
-  }
+    @Query(() => [Item], { name: 'items' })
+    findAll() {
+        return this.itemsService.findAll();
+    }
 
-  @Query(() => Item, { name: 'item' })
-  findOne(@Args('id', { type: () => Int }) id: number) {
-    return this.itemsService.findOne(id);
-  }
+    @Query(() => Item, { name: 'item' })
+    findOne(@Args('id', { type: () => Int }) id: number) {
+        return this.itemsService.findOne(id);
+    }
 
-  @Mutation(() => Item)
-  updateItem(@Args('updateItemInput') updateItemInput: UpdateItemInput) {
-    return this.itemsService.update(updateItemInput.id, updateItemInput);
-  }
+    @Mutation(() => Item)
+    updateItem(@Args('updateItemInput') updateItemInput: UpdateItemInput) {
+        return this.itemsService.update(updateItemInput.id, updateItemInput);
+    }
 
-  @Mutation(() => Item)
-  removeItem(@Args('id', { type: () => Int }) id: number) {
-    return this.itemsService.remove(id);
-  }
+    @Mutation(() => Item)
+    removeItem(@Args('id', { type: () => Int }) id: number) {
+        return this.itemsService.remove(id);
+    }
 }
