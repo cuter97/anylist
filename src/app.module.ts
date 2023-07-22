617d660a133ebc3032fb06e41e2e8e239419417b
cuter97 <aguscastets@gmail.com>
2023-07-22 18:49:53 -0300

    block gqlschema - graphqlModule forRootAsync

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,29 +8,52 @@ import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin
 import { ItemsModule } from './items/items.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
     imports: [
         ConfigModule.forRoot(),
 
-        GraphQLModule.forRoot<ApolloDriverConfig>({
+        GraphQLModule.forRootAsync({
             driver: ApolloDriver,
-            // debug: false,
-            playground: false,
-            autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-            plugins: [ApolloServerPluginLandingPageLocalDefault()],
+            imports: [AuthModule],
+            inject: [JwtService],
+            useFactory: async (jwtService: JwtService) => {
+                return {
+                    playground: false,
+                    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+                    plugins: [ApolloServerPluginLandingPageLocalDefault()],
+                    context({ req }) { 
+                        // const token = req.headers.authorization?.replace('Bearer ', '');
+                        // if(!token) throw new Error('Token needed');
+                        
+                        // const payload = jwtService.decode(token);
+                        // if(!payload) throw new Error('Token not valid');
+                    }
+                }
+            }
         }),
+
+
+        //config basica
+        // GraphQLModule.forRoot<ApolloDriverConfig>({
+        //     driver: ApolloDriver,
+        //     // debug: false,
+        //     playground: false,
+        //     autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+        //     plugins: [ApolloServerPluginLandingPageLocalDefault()],
+        // }),
 
         TypeOrmModule.forRoot({
             type: 'postgres',
             host: process.env.DB_HOST,
             port: +process.env.DB_PORT,
             username: process.env.DB_USERNAME,
-            password:  process.env.DB_PASSWORD,
+            password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
             synchronize: true,
             autoLoadEntities: true,
-          }),
+        }),
 
         ItemsModule,
 
