c6872688ba504c897b1bd3bbda9178f1f2d526b6
cuter97 <aguscastets@gmail.com>
2023-07-26 20:19:30 -0300

    add findOne ListItem query

--- src/list-item/list-item.resolver.ts
+++ src/list-item/list-item.resolver.ts
@@ -1,5 +1,5 @@
 import { Resolver, Query, Mutation, Args, Int } from '@nestjs/graphql';
-import { UseGuards } from '@nestjs/common';
+import { ParseUUIDPipe, UseGuards } from '@nestjs/common';
 
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 
@@ -21,15 +21,10 @@ export class ListItemResolver {
         return this.listItemService.create(createListItemInput);
     }
 
-    // @Query(() => [ListItem], { name: 'listItem' })
-    // findAll() {
-    //     return this.listItemService.findAll();
-    // }
-
-    // @Query(() => ListItem, { name: 'listItem' })
-    // findOne(@Args('id', { type: () => Int }) id: number) {
-    //   return this.listItemService.findOne(id);
-    // }
+    @Query(() => ListItem, { name: 'listItem' })
+    async findOne(@Args('id', { type: () => String }, ParseUUIDPipe) id: string): Promise<ListItem> {
+      return this.listItemService.findOne(id);
+    }
 
     // @Mutation(() => ListItem)
     // updateListItem(@Args('updateListItemInput') updateListItemInput: UpdateListItemInput) {
